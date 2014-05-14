d9826b641aaad706c593a1b3e4170c5fc29b8088
Sid Raval <sidsraval@gmail.com>
2014-05-14 12:22:59 -0700

    Better way to move resize arrow with window.

--- src/image_crop/core.cljs
+++ src/image_crop/core.cljs
@@ -1,11 +1,12 @@
 (ns image-crop.core
-  (:use [jayq.core :only [$ width on document-ready css off position]]))
+  (:use [jayq.core :only [$ width height on document-ready css off position]]))
 
 (enable-console-print!)
 
 (def aspect-ratio 2)
 (defn img [] ($ :#image))
 (defn box [] ($ :#frame))
+(defn resize [] ($ :#resize))
 
 (defn move-box [x y e]
   (do
@@ -24,10 +25,23 @@
 (defn unbind-mousemove []
   (off (box) :mousemove))
 
+(defn bind-resize []
+  (do
+    (on (img) :mousemove #(println "YES"))
+    false))
+
+(defn change-width []
+  )
+
+(defn unbind-resize []
+  (off (img) :mousemove))
+
 (defn bind-mousedown []
   (do
     (on (box) :mousedown bind-mousemove)
-    (on (box) :mouseup unbind-mousemove)))
+    (on (box) :mouseup unbind-mousemove)
+    (on (resize) :mousedown bind-resize)
+    (on (resize) :mouseup unbind-resize)))
 
 (defn initialize []
   (bind-mousedown))
